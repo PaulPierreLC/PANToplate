79adb06894a34ce0402128dbd1dedac254285a83
Paul Le Coz <plecoz.pro@gmail.com>
2025-05-12 15:23:08 +0200

    Removed console log

--- assets/scripts/commande/header.js
+++ assets/scripts/commande/header.js
@@ -5,8 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const adresseNeededPages = ["/src/commande/restaurants.html", "/src/commande/restaurant.html", "/src/commande/commande.html"];
 
-	console.log(window.location.pathname);
-
 	if (adresseNeededPages.includes(window.location.pathname) && (!adresse || !heure)) {
 		window.location.href = "adresse.html";
 		sessionStorage.setItem("mauvaiseAdresse", "true");
